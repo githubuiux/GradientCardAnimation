b580cc0856614d0faa61d21743791e649be1648f
githubuiux <uiuxista@gmail.com>
2023-07-07 22:48:38 +0300

    Initial Commit

---
+++ GradientCardAnimation/GradientCardAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientCardAnimationApp.swift
+//  GradientCardAnimation
+//
+//  Created by Levent on 7.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct GradientCardAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
